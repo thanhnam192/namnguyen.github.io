2131194130ab8c287240312f832df6103ac9ddd1
phuchieuct <phuchieuct93@gmail.com>
2017-04-29 10:47:34 +0700

    Add dom_manipulation

--- controllers/ctr4.js
+++ controllers/ctr4.js
@@ -1,5 +1,4 @@
 app.controller("ctr4",function($scope){
-	$scope.mvcState = "state1"
 	$scope.click=function(feature,state){
 		$scope[feature]=state
 
--- controllers/dom_manipulation.js
+++ controllers/dom_manipulation.js
@@ -1,4 +1,13 @@
 app.controller("domManipulation",function($scope){
-	
-	
+	$scope.classes=['text-primary','text-danger','text-success']
+    $scope.class='text-primary';
+	$scope.getClass=function(classString){
+	    try{
+            return JSON.parse(classString)
+
+        }catch(exception){
+	        return {};
+        }
+
+    }
 })
