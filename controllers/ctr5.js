f0df038e1a43027c3d25a7dfa06dff54bf280d11
phuchieuct <phuchieuct93@gmail.com>
2017-04-30 15:06:58 +0700

    Update css

--- controllers/ctr5.js
+++ controllers/ctr5.js
@@ -1,35 +1,32 @@
-app.controller("ctr5",function($scope){
-	
-	$scope.table1 = [
-	{
-		name:"JavaScript",
-		value:"90"
-	},
-		{
-		name:"AngularJS",
-		value:"40"
-	},
-		{
-		name:"PHP",
-		value:"30"
-	},
-		{
-		name:"PHP",
-		value:"35"
-	},
-		{
-		name:"NodeJS",
-		value:"32"
-	},
-		{
-		name:"Wordpress",
-		value:"30"
-	},
-	{
-		name:"React",
-		value:"28"
-	}
-	
+app.controller("ctr5", function ($scope) {
 
-	]
+    $scope.table1 = [
+        {
+            name: "JavaScript",
+            value: "90"
+        },
+        {
+            name: "AngularJS",
+            value: "40"
+        },
+        {
+            name: "PHP",
+            value: "30"
+        },
+
+        {
+            name: "NodeJS",
+            value: "32"
+        },
+        {
+            name: "Wordpress",
+            value: "30"
+        },
+        {
+            name: "React",
+            value: "28"
+        }
+
+
+    ]
 });
