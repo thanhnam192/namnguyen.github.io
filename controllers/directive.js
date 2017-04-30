29602427911585b6013f4c57a07ece6f071520cc
phuchieuct <phuchieuct93@gmail.com>
2017-04-30 16:19:20 +0700

    Update css

--- controllers/directive.js
+++ controllers/directive.js
@@ -1,18 +1,18 @@
 app.directive("searchField",function(){
     return {
         restrict : 'E',
-        scope:{},
+        scope:{value:"@"},
         template : '<div class="form-inline input-group-lg">' +
             '<input type="text" ng-model="value" class="nam_text form-control" />' +
-            '<button class="nam_btn btn btn-primary btn-lg"  ng-click="onSubmit()">Search</button>' +
+            '<button class="nam_btn btn btn-primary btn-lg" ng-click="onSubmit()" >Search</button>' +
             '</div>' + 
             '<b>{{result}}</b>',
         controller : function($scope){
-            $scope.value = '';
             $scope.result = 'Result: ';
             $scope.onSubmit = function(){
                 $scope.result = 'Result: ' + $scope.value;
             }
+            $scope.onSubmit();
         }
     }
 })
