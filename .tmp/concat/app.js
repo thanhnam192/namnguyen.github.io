2057c3a3c3c042572de9eaf303af9a1d9faff587
phuchieuct <phuchieuct93@gmail.com>
2017-05-07 19:25:17 +0700

    Flatten slide (no horizontal slides any longer)

--- .tmp/concat/app.js
+++ .tmp/concat/app.js
@@ -142,6 +142,27 @@ app.controller("liveCodeInstallation", function ($timeout) {
 
 
 })
+app.controller("liveCodeTwoWay", function ($timeout) {
+    var mySwiper;
+    Reveal.addEventListener('liveCodeTwoWay', function () {
+        if (mySwiper == null) {
+            $timeout(function () {
+                new Swiper('.live-code-twoway .swiper-container', {
+                    speed: 1000,
+                    spaceBetween: 100,
+                    nextButton: ".live-code-twoway .swiper-button-next",
+                    prevButton: ".live-code-twoway .swiper-button-prev",
+
+                })
+            }, 1000);
+            $('pre code').each(function (i, block) {
+                hljs.highlightBlock(block);
+            });
+        }
+    }, false);
+
+
+})
 app.controller('ctrlMVC', function ($timeout) {
   
             $timeout(function () {
